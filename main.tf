1a159cc0f96fe9b484b27d802ff2cf5e5ad03f25
parthkumar141 <81438758+parthkumar141@users.noreply.github.com>
2024-06-18 14:04:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,58 +1,57 @@
-//vpc has been created through ui
-data "aws_vpc" "existing_vpc" {
-  id = "vpc-0df5066ec2d11c46a" 
+provider "aws" {
+  region = "us-east-1"
 }
 
-
-data "aws_subnet" "subnet1" {
-  id = "subnet-08079ca45b9540514" 
+variable "aws_access_key"{
+}
+variable "aws_secret" {
 }
 
-data "aws_subnet" "subnet2" {
-  id = "subnet-04f443d54a1a2eb7d"  
+variable "s3_buckets" {
+  description = "List of S3 buckets with their attributes"
+  type = list(object({
+    name = string
+    acl  = string
+  }))
+  default = [
+    {
+      name = "example-a1"
+      acl  = "private"
+    },
+    {
+      name = "example-b1"
+      acl  = "private"
+    },
+    {
+      name = "example-c1"
+      acl  = "private"
+    }
+  ]
 }
 
-resource "aws_security_group" "instance_sg" {
-  vpc_id = data.aws_vpc.existing_vpc.id
+resource "aws_s3_bucket" "a1" {
+  bucket = var.s3_buckets[0].name
+  acl    = var.s3_buckets[0].acl
+}
 
-  
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_s3_bucket" "b1" {
+  bucket = var.s3_buckets[1].name
+  acl    = var.s3_buckets[1].acl
+}
+
+resource "aws_s3_bucket" "c1" {
+  bucket = var.s3_buckets[2].name
+  acl    = var.s3_buckets[2].acl
+}
+
+locals {
+  s3_buckets_map = {
+    "example-a1" = var.s3_buckets[0]
+    "example-b1" = var.s3_buckets[1]
+    "example-c1" = var.s3_buckets[2]
   }
-
-  
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
-
-resource "aws_instance" "parth" {
-  ami           = "ami-08a0d1e16fc3f61ea" 
-  instance_type = "t2.micro"
-  subnet_id     =  "subnet-08079ca45b9540514" 
-  key_name      = "parthbox"  
-  security_groups = [aws_security_group.instance_sg.id]
-
- 
-  
-}
-
-
-output "instance_public_ip" {
-  value = aws_instance.example.public_ip
+output "c1_bucket_details" {
+  value = lookup(local.s3_buckets_map, "example-c1")
 }
