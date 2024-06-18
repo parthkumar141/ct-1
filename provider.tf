d2f139ef562b5864d5f665eb261d744186a82543
parthkumar141 <81438758+parthkumar141@users.noreply.github.com>
2024-06-18 14:03:03 +0530

    Delete provider.tf

--- provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "us-east-1" 
- credentials file
-   profile = "default"
-   access_key = ""
-   secret_key = ""
-}
-// aws credentials have been removed due to security reasons
