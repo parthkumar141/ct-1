80db1437698785603195b04eced3254aa1a71aec
parthkumar141 <81438758+parthkumar141@users.noreply.github.com>
2024-06-18 13:56:03 +0530

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,16 +1,52 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
+}
+
+variable "s3_buckets" {
+  description = "List of S3 buckets with their attributes"
+  type = list(object({
+    name = string
+    acl  = string
+  }))
+  default = [
+    {
+      name = "example-a1"
+      acl  = "private"
+    },
+    {
+      name = "example-b1"
+      acl  = "private"
+    },
+    {
+      name = "example-c1"
+      acl  = "private"
+    }
+  ]
+}
+
+resource "aws_s3_bucket" "a1" {
+  bucket = var.s3_buckets[0].name
+  acl    = var.s3_buckets[0].acl
+}
+
+resource "aws_s3_bucket" "b1" {
+  bucket = var.s3_buckets[1].name
+  acl    = var.s3_buckets[1].acl
+}
+
+resource "aws_s3_bucket" "c1" {
+  bucket = var.s3_buckets[2].name
+  acl    = var.s3_buckets[2].acl
 }
 
 locals {
-  bucket_names = ["macbook", "mouse", "laptop", "parth", "dell"]
+  s3_buckets_map = {
+    "example-a1" = var.s3_buckets[0]
+    "example-b1" = var.s3_buckets[1]
+    "example-c1" = var.s3_buckets[2]
+  }
 }
 
-resource "aws_s3_bucket" "example_buckets" {
-  count  = length(local.bucket_names)
-  bucket = local.bucket_names[count.index]
-}
-
-output "parth_bucket_id" {
-  value = lookup({ for b in aws_s3_bucket.example_buckets : b.bucket => b.id if b.bucket == "parth" }, "parth", "Bucket not found")
+output "c1_bucket_details" {
+  value = lookup(local.s3_buckets_map, "example-c1")
 }
